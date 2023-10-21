0bca9e0cf5e8a68c5fc3519bf06d02ecedb7b501
Mitchell Goff <mitchellgoffpc@gmail.com>
2023-10-21 11:30:22 -0700

    Moved query function into its own file

--- ask/main.py
+++ ask/main.py
@@ -1,39 +1,12 @@
 #!/usr/bin/env python3
-import os
 import sys
-import json
 import argparse
-import requests
-from typing import List, Dict, Union
+from ask.query import query
 
 MODEL_SHORTCUTS = {'gpt3': 'gpt-3.5-turbo', 'gpt4': 'gpt-4', '3': 'gpt-3.5-turbo', '4': 'gpt-4'}
 LANGUAGE_SHORTCUTS = {'fr': 'french', 'es': 'spanish'}
 AVAILABLE_MODELS = ['gpt-3.5-turbo', 'gpt-4', *MODEL_SHORTCUTS]
 DEFAULT_MODEL = 'gpt-3.5-turbo'
-
-Message = Union[str, List[Dict[str, str]]]
-
-def query(message: Message, model: str = DEFAULT_MODEL):
-  assert isinstance(message, (list, str))
-  if isinstance(message, str):
-    message = [{"role": "user", "content": message}]
-
-  api_key = os.getenv('OPENAI_API_KEY')
-  headers = {"Authorization": f"Bearer {api_key}"}
-  params = {"model": model, "messages": message, "temperature": 0.7}
-
-  assert api_key, "OPENAI_API_KEY environment variable isn't set!"
-  r = requests.post('https://api.openai.com/v1/chat/completions', timeout=None, headers=headers, json=params)
-
-  result = r.json()
-  if r.status_code != 200:
-    print(json.dumps(result, indent=2))
-    raise RuntimeError("Invalid response from API")
-  if os.getenv("DEBUG"):
-    print(json.dumps(result, indent=2))
-  assert len(result['choices']) == 1, f"Expected exactly one choice, but got {len(result['choices'])}!"
-
-  return result['choices'][0]['message']['content']
 
 
 def main():
