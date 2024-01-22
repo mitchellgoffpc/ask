35ccb124c2a5843b6fe83810a7e52f34d170e450
Mitchell Goff <mitchellgoffpc@gmail.com>
2024-01-21 21:24:32 -0800

    Added support for multiple files

--- ask/main.py
+++ ask/main.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
 import sys
+import glob
 import argparse
+import itertools
+from pathlib import Path
 from ask.query import query
 from ask.models import MODELS
 
 MODEL_SHORTCUTS = {s: model for model in MODELS for s in [model.name, *model.shortcuts]}
 LANGUAGE_SHORTCUTS = {'fr': 'french', 'es': 'spanish'}
 
+def read_file(path):
+  with open(path) as f:
+    return f.read().strip()
+
+def list_files(path):
+  if path.name.startswith('.'):
+    return []
+  elif path.is_file():
+    return [path]
+  elif path.is_dir():
+    return list(itertools.chain.from_iterable(list_files(child) for child in path.iterdir()))
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('-m', '--model', choices=MODEL_SHORTCUTS.keys(), default='gpt-3.5-turbo')
-  parser.add_argument('-f', '--file')
+  parser.add_argument('-f', '--file', action='append', default=[])
   parser.add_argument('-c', '--context')
   parser.add_argument('-t', '--translate')
   parser.add_argument('question', nargs=argparse.REMAINDER)
@@ -20,9 +34,6 @@ def main():
 
   if not sys.stdin.isatty():
     question = parser.parse_args().stdin.read()
-  elif args.file:
-    with open(args.file) as f:
-      question = f.read()
   else:
     question = ' '.join(args.question)
 
@@ -30,10 +41,12 @@ def main():
   if args.translate:
     language = LANGUAGE_SHORTCUTS.get(args.translate, args.translate)
     question = f'How do I translate "{question}" into {language}?'
-  if args.context:
-    with open(args.context) as f:
-      context = f.read().strip()
-      question = f"{question}\n\n```\n{context}\n```"
+  if args.file:
+    file_paths = list(itertools.chain.from_iterable(glob.glob(fn) for fn in args.file))
+    file_paths = list(itertools.chain.from_iterable(list_files(Path(fn)) for fn in file_paths))
+    file_data = {path: read_file(path) for path in file_paths}
+    context = '\n\n'.join(f'{path}\n```\n{data}\n```' for path, data in file_data.items())
+    question = f"{context}\n\n{question}"
 
   response = query(question, MODEL_SHORTCUTS[args.model])
   print(response)
