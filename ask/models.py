f0d2efe5e4379f2af491035aa3f4079cbe785ca5
Mitchell Goff <mitchellgoffpc@gmail.com>
2024-02-09 02:00:46 -0800

    Added gpt-4-turbo

--- ask/models.py
+++ ask/models.py
@@ -19,5 +19,6 @@ APIS = {
 MODELS = [
   Model(name='gpt-3.5-turbo', api='openai', shortcuts=['gpt3', '3']),
   Model(name='gpt-4', api='openai', shortcuts=['gpt4', '4']),
+  Model(name='gpt-4-turbo-preview', api='openai', shortcuts=['gpt4t', '4t', 't']),
   Model(name='mistral-medium', api='mistral', shortcuts=['mistral', 'i']),
 ]
