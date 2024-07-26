b6f710f0ef2a8dafd2f63621b8361f55ebf68fed
Mitchell Goff <mitchellgoffpc@gmail.com>
2024-07-25 22:07:44 -0700

    Added Groq + Llama 3 to models.py

--- ask/models.py
+++ ask/models.py
@@ -57,6 +57,7 @@ class Model:
 APIS = {
     'openai': API(url='https://api.openai.com/v1/chat/completions', key='OPENAI_API_KEY'),
     'mistral': API(url='https://api.mistral.ai/v1/chat/completions', key='MISTRAL_API_KEY'),
+    'groq': API(url='https://api.groq.com/openai/v1/chat/completions', key='GROQ_API_KEY'),
     'anthropic': AnthropicAPI(url='https://api.anthropic.com/v1/messages', key='ANTHROPIC_API_KEY'),
 }
 
@@ -65,9 +66,12 @@ MODELS = [
     Model(name='gpt-4', api=APIS['openai'], shortcuts=['gpt4', '4']),
     Model(name='gpt-4-turbo', api=APIS['openai'], shortcuts=['gpt4t', '4t', 't']),
     Model(name='gpt-4o', api=APIS['openai'], shortcuts=['gpt4o', '4o', 'o']),
-    Model(name='open-mixtral-8x7b', api=APIS['mistral'], shortcuts=['mixtral', 'x']),
-    Model(name='mistral-medium-latest', api=APIS['mistral'], shortcuts=['mistral-med', 'i']),
-    Model(name='mistral-large-latest', api=APIS['mistral'], shortcuts=['mistral-large', 'l']),
+    Model(name='open-mixtral-8x7b', api=APIS['mistral'], shortcuts=['mixtral', 'mx']),
+    Model(name='mistral-medium-latest', api=APIS['mistral'], shortcuts=['mistral-med', 'md']),
+    Model(name='mistral-large-latest', api=APIS['mistral'], shortcuts=['mistral-large', 'ml', 'i']),
+    Model(name='llama-3.1-8b-instant', api=APIS['groq'], shortcuts=['llama-small', 'llama-8b', 'ls', 'l8']),
+    Model(name='llama-3.1-70b-versatile', api=APIS['groq'], shortcuts=['llama-med', 'llama-70b', 'lm', 'l70']),
+    Model(name='llama-3.1-405b-reasoning', api=APIS['groq'], shortcuts=['llama-large', 'llama-405b', 'll', 'l405', 'l']),
     Model(name='claude-3-haiku-20240307', api=APIS['anthropic'], shortcuts=['haiku', 'h']),
     Model(name='claude-3-5-sonnet-20240620', api=APIS['anthropic'], shortcuts=['sonnet', 's']),
     Model(name='claude-3-opus-20240229', api=APIS['anthropic'], shortcuts=['claude', 'opus', 'c']),
