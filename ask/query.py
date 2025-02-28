ed5ef23ea0465eadf4728912e1b087ce70a42fbe
Mitchell Goff <mitchellgoffpc@gmail.com>
2025-02-27 21:31:42 -0800

    Fix broken query function

--- ask/query.py
+++ ask/query.py
@@ -12,16 +12,18 @@ def query(model: Model, messages: list[Message], tools: list[Tool], system_promp
     headers = api.headers(api_key)
     assert api_key, f"{api.key!r} environment variable isn't set!"
     with requests.post(api.url, timeout=None, headers=headers, json=params, stream=model.stream) as r:
-        try:
-            result = r.json()
-        except requests.exceptions.JSONDecodeError as e:
-            print(r.text)
-            raise RuntimeError("Invalid response from API") from e
         if r.status_code != 200:
-            print(json.dumps(result, indent=2))
+            try:
+                print(json.dumps(r.json(), indent=2))
+            except requests.exceptions.JSONDecodeError:
+                print(r.text)
             raise RuntimeError("Invalid response from API")
         if model.stream:
             yield from api.decode(line.decode('utf-8') for line in r.iter_lines())
         else:
-            for item in api.result(r.json()):
-                yield '', item
+            try:
+                for item in api.result(r.json()):
+                    yield '', item
+            except requests.exceptions.JSONDecodeError as e:
+                print(r.text)
+                raise RuntimeError("Invalid response from API") from e
