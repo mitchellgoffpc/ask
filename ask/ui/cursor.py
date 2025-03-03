97599ee489442ac3216ee5ab2db4ea77ea89c304
Mitchell Goff <mitchellgoffpc@gmail.com>
2025-03-02 17:22:07 -0800

    Started work on ui

---
+++ ask/ui/cursor.py
@@ -0,0 +1,105 @@
+import os
+import sys
+import atexit
+import platform
+from typing import Optional, TextIO
+
+ESC = '\u001B['
+OSC = '\u001B]'
+BEL = '\u0007'
+SEP = ';'
+
+is_terminal_app = os.environ.get('TERM_PROGRAM') == 'Apple_Terminal'
+is_windows = platform.system() == 'win32'
+
+cursor_left = f"{ESC}G"
+cursor_save_position = '\u001B7' if is_terminal_app else f"{ESC}s"
+cursor_restore_position = '\u001B8' if is_terminal_app else f"{ESC}u"
+cursor_get_position = f"{ESC}6n"
+cursor_next_line = f"{ESC}E"
+cursor_prev_line = f"{ESC}F"
+cursor_hide = f"{ESC}?25l"
+cursor_show = f"{ESC}?25h"
+
+erase_end_line = f"{ESC}K"
+erase_start_line = f"{ESC}1K"
+erase_line = f"{ESC}2K"
+erase_down = f"{ESC}J"
+erase_up = f"{ESC}1J"
+erase_screen = f"{ESC}2J"
+scroll_up = f"{ESC}S"
+scroll_down = f"{ESC}T"
+
+clear_screen = '\u001Bc'
+clear_terminal = f"{erase_screen}{ESC}0f" if is_windows else f"{erase_screen}{ESC}3J{ESC}H"
+
+enter_alternative_screen = f"{ESC}?1049h"
+exit_alternative_screen = f"{ESC}?1049l"
+
+
+# Show / hide cursor
+
+def show_cursor(writable_stream: TextIO = sys.stderr) -> None:
+    if not hasattr(writable_stream, 'isatty') or not writable_stream.isatty():
+        return
+    writable_stream.write(cursor_show)
+    writable_stream.flush()
+
+def hide_cursor(writable_stream: TextIO = sys.stderr) -> None:
+    if not hasattr(writable_stream, 'isatty') or not writable_stream.isatty():
+        return
+    writable_stream.write(cursor_hide)
+    writable_stream.flush()
+
+def show_cursor_on_exit():
+    sys.stderr.write(cursor_show)
+    sys.stderr.flush()
+
+
+# Move cursor
+
+def cursor_to(x: int, y: Optional[int] = None) -> str:
+    if y is None:
+        return f"{ESC}{x + 1}G"
+    return f"{ESC}{y + 1}{SEP}{x + 1}H"
+
+def cursor_move(x: int, y: int = 0) -> str:
+    return_value = ''
+
+    if x < 0:
+        return_value += f"{ESC}{-x}D"
+    elif x > 0:
+        return_value += f"{ESC}{x}C"
+
+    if y < 0:
+        return_value += f"{ESC}{-y}A"
+    elif y > 0:
+        return_value += f"{ESC}{y}B"
+
+    return return_value
+
+def cursor_up(count: int = 1) -> str:
+    return f"{ESC}{count}A"
+
+def cursor_down(count: int = 1) -> str:
+    return f"{ESC}{count}B"
+
+def cursor_forward(count: int = 1) -> str:
+    return f"{ESC}{count}C"
+
+def cursor_backward(count: int = 1) -> str:
+    return f"{ESC}{count}D"
+
+def erase_lines(count: int) -> str:
+    clear = ''
+    for i in range(count):
+        clear += erase_line + (cursor_up() if i < count - 1 else '')
+    if count:
+        clear += cursor_left
+    return clear
+
+def link(text: str, url: str) -> str:
+    return f"{OSC}8{SEP}{SEP}{url}{BEL}{text}{OSC}8{SEP}{SEP}{BEL}"
+
+
+atexit.register(show_cursor_on_exit)
---
+++ ask/ui/styles.py
@@ -0,0 +1,162 @@
+import os
+import re
+from functools import partial
+
+ANSI_BACKGROUND_OFFSET = 10
+ANSI_256_SUPPORT = '256color' in os.getenv("TERM", '')
+ANSI_16M_SUPPORT = 'truecolor' in os.getenv("COLORTERM", '') or '24bit' in os.getenv("COLORTERM", '')
+
+def ansi16(code: int, *, offset: int = 0) -> str:
+    return f"\u001B[{code + offset}m"
+
+def ansi256(code: int, *, offset: int = 0) -> str:
+    return f"\u001B[{38 + offset};5;{code}m"
+
+def ansi16m(red: int, green: int, blue: int, *, offset: int = 0) -> str:
+    return f"\u001B[{38 + offset};2;{red};{green};{blue}m"
+
+def rgb_to_ansi256(red: int, green: int, blue: int) -> int:
+    # From https://github.com/Qix-/color-convert/blob/3f0e0d4e92e235796ccb17f6e85c72094a651f49/conversions.js
+    # We use the extended greyscale palette here, with the exception of
+    # black and white. normal palette only has 4 greyscale shades.
+    if red == green and green == blue:
+        if red < 8:
+            return 16
+        if red > 248:
+            return 231
+        return round(((red - 8) / 247) * 24) + 232
+
+    return 16 + (36 * round(red / 255 * 5)) + (6 * round(green / 255 * 5)) + round(blue / 255 * 5)
+
+def hex_to_rgb(hex_str: str) -> tuple[int, int, int]:
+    matches = re.search(r'[a-f\d]{6}|[a-f\d]{3}', str(hex_str), re.IGNORECASE)
+    if not matches:
+        return (0, 0, 0)
+
+    color_string = matches.group(0)
+    if len(color_string) == 3:
+        color_string = ''.join([c + c for c in color_string])
+
+    integer = int(color_string, 16)
+    return (integer >> 16) & 0xFF, (integer >> 8) & 0xFF, integer & 0xFF
+
+def hex_to_ansi256(hex_str: str) -> int:
+    rgb = hex_to_rgb(hex_str)
+    return rgb_to_ansi256(rgb[0], rgb[1], rgb[2])
+
+def ansi256_to_ansi(code: int) -> int:
+    if code < 8:
+        return 30 + code
+    if code < 16:
+        return 90 + (code - 8)
+
+    if code >= 232:
+        red = (((code - 232) * 10) + 8) / 255
+        green = red
+        blue = red
+    else:
+        code -= 16
+        remainder = code % 36
+        red = (code // 36) / 5
+        green = (remainder // 6) / 5
+        blue = (remainder % 6) / 5
+
+    value = max(red, green, blue) * 2
+    if value == 0:
+        return 30
+
+    result = 30 + ((round(blue) << 2) | (round(green) << 1) | round(red))
+    if value == 2:
+        result += 60
+
+    return result
+
+def rgb_to_ansi(red: int, green: int, blue: int) -> int:
+    return ansi256_to_ansi(rgb_to_ansi256(red, green, blue))
+
+def hex_to_ansi(hex_str: str) -> int:
+    return ansi256_to_ansi(hex_to_ansi256(hex_str))
+
+def rgb_to_best_ansi(red: int, green: int, blue: int, *, offset: int = 0) -> str:
+    if ANSI_16M_SUPPORT:
+        return ansi16m(red, green, blue, offset=offset)
+    elif ANSI_256_SUPPORT:
+        code = rgb_to_ansi256(red, green, blue)
+        return ansi256(code, offset=offset)
+    else:
+        code = rgb_to_ansi(red, green, blue)
+        return ansi16(code, offset=offset)
+
+def hex_to_best_ansi(hex_str: str, *, offset: int = 0) -> str:
+    return rgb_to_best_ansi(*hex_to_rgb(hex_str), offset=offset)
+
+
+class Colors:
+    # Colors
+    BLACK = "\u001B[30m"
+    RED = "\u001B[31m"
+    GREEN = "\u001B[32m"
+    YELLOW = "\u001B[33m"
+    BLUE = "\u001B[34m"
+    MAGENTA = "\u001B[35m"
+    CYAN = "\u001B[36m"
+    WHITE = "\u001B[37m"
+    BLACK_BRIGHT = "\u001B[90m"
+    RED_BRIGHT = "\u001B[91m"
+    GREEN_BRIGHT = "\u001B[92m"
+    YELLOW_BRIGHT = "\u001B[93m"
+    BLUE_BRIGHT = "\u001B[94m"
+    MAGENTA_BRIGHT = "\u001B[95m"
+    CYAN_BRIGHT = "\u001B[96m"
+    WHITE_BRIGHT = "\u001B[97m"
+    END = "\u001B[39m"
+
+    # Background colors
+    BG_BLACK = "\u001B[40m"
+    BG_RED = "\u001B[41m"
+    BG_GREEN = "\u001B[42m"
+    BG_YELLOW = "\u001B[43m"
+    BG_BLUE = "\u001B[44m"
+    BG_MAGENTA = "\u001B[45m"
+    BG_CYAN = "\u001B[46m"
+    BG_WHITE = "\u001B[47m"
+    BG_BLACK_BRIGHT = "\u001B[100m"
+    BG_RED_BRIGHT = "\u001B[101m"
+    BG_GREEN_BRIGHT = "\u001B[102m"
+    BG_YELLOW_BRIGHT = "\u001B[103m"
+    BG_BLUE_BRIGHT = "\u001B[104m"
+    BG_MAGENTA_BRIGHT = "\u001B[105m"
+    BG_CYAN_BRIGHT = "\u001B[106m"
+    BG_WHITE_BRIGHT = "\u001B[107m"
+    BG_END = "\u001B[49m"
+
+    # Styles
+    RESET = "\u001B[0m"
+    BOLD = "\u001B[1m"
+    DIM = "\u001B[2m"
+    ITALIC = "\u001B[3m"
+    UNDERLINE = "\u001B[4m"
+    OVERLINE = "\u001B[53m"
+    INVERSE = "\u001B[7m"
+    HIDDEN = "\u001B[8m"
+    STRIKETHROUGH = "\u001B[9m"
+    BOLD_END = "\u001B[22m"
+    DIM_END = "\u001B[22m"
+    ITALIC_END = "\u001B[23m"
+    UNDERLINE_END = "\u001B[24m"
+    OVERLINE_END = "\u001B[55m"
+    INVERSE_END = "\u001B[27m"
+    HIDDEN_END = "\u001B[28m"
+    STRIKETHROUGH_END = "\u001B[29m"
+
+    ansi = staticmethod(ansi16)
+    ansi256 = staticmethod(ansi256)
+    ansi16m = staticmethod(ansi16m)
+    bg_ansi = staticmethod(partial(ansi16, offset=ANSI_BACKGROUND_OFFSET))
+    bg_ansi256 = staticmethod(partial(ansi256, offset=ANSI_BACKGROUND_OFFSET))
+    bg_ansi16m = staticmethod(partial(ansi16m, offset=ANSI_BACKGROUND_OFFSET))
+
+    hex = staticmethod(hex_to_best_ansi)
+    rgb = staticmethod(rgb_to_best_ansi)
+    bg_hex = staticmethod(partial(hex_to_best_ansi, offset=ANSI_BACKGROUND_OFFSET))
+    bg_rgb = staticmethod(partial(rgb_to_best_ansi, offset=ANSI_BACKGROUND_OFFSET))
