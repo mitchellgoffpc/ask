9c7fc7b0b71393507bc1e0992d566bcbb6013206
Mitchell Goff <mitchellgoffpc@gmail.com>
2025-08-02 22:53:37 -0700

    Added config class

---
+++ ask/ui/config.py
@@ -0,0 +1,19 @@
+import json
+from pathlib import Path
+
+CONFIG_PATH = Path('~/.ask.json').expanduser()
+
+class Config:
+    def __init__(self):
+        if CONFIG_PATH.is_file():
+            self.data = json.loads(CONFIG_PATH.read_text() or '{}')
+        if not self.data:
+            self.data = {'history': []}
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+        with open(CONFIG_PATH, 'w') as f:
+            json.dump(self.data, f, indent=2)
