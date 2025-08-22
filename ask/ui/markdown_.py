3a8961a9c5964469a9b122f85db32cd23cb7ac78
Mitchell Goff <mitchellgoffpc@gmail.com>
2025-08-22 09:10:16 -0700

    Manual spacing between block components in markdown.py

--- ask/ui/markdown_.py
+++ ask/ui/markdown_.py
@@ -21,7 +21,7 @@ class FencedCodeExtension(Extension):
 
 class FencedCodeBlockProcessor(BlockProcessor):
     RE_FENCE_START = r'(?:^|\n)```(?P<lang>[\w#.+-]*) *(?=\n)'
-    RE_FENCE_END = r'\n```\s*$'
+    RE_FENCE_END = r'\n```$'
 
     def test(self, parent, block):
         return re.search(self.RE_FENCE_START, block)
@@ -42,7 +42,7 @@ class FencedCodeBlockProcessor(BlockProcessor):
                 blocks[block_num] = re.sub(self.RE_FENCE_END, '', blocks[block_num])
                 e = SubElement(parent, 'pre')
                 e.set('language', match.group('lang'))
-                e.text = AtomicString('\n'.join(blocks[:block_num + 1]))
+                e.text = AtomicString('\n\n'.join(blocks[: block_num + 1]))
                 for _ in range(0, block_num + 1):
                     blocks.pop(0)
                 return True
@@ -53,6 +53,7 @@ class FencedCodeBlockProcessor(BlockProcessor):
 # ANSI rendering extension
 
 class ANSIExtension(Extension):
+    BLOCK_TAGS = {'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'ul', 'ol', 'li', 'pre', 'blockquote'}
     HTML_TO_ANSI = {
         'strong': (Styles.BOLD, Styles.BOLD_END),
         'em': (Styles.ITALIC, Styles.ITALIC_END),
@@ -76,30 +77,40 @@ class ANSIExtension(Extension):
             lexer = get_lexer_by_name(language)
         except ClassNotFound:
             lexer = guess_lexer(code)
-        return cast(str, highlight(code, lexer, formatter)) + (element.tail or '')
+        return cast(str, highlight(code, lexer, formatter)) + (element.tail or "")
 
     def render_basic_element(self, element: Element, stream: StringIO, indent: int) -> None:
         start, end = self.HTML_TO_ANSI.get(element.tag, ('', ''))
         stream.write(start)
+        if element.text and element.tag in self.BLOCK_TAGS:
+            element.text = element.text.lstrip('\n')
         if element.text:
             stream.write(element.text)
+            if len(element) and list(element)[0].tag in self.BLOCK_TAGS:
+                stream.write('\n')
+
+        prev_tag = None
         for sub in element:
+            if prev_tag:
+                if prev_tag == 'li' and sub.tag == 'li':
+                    stream.write('\n')
+                elif prev_tag in self.BLOCK_TAGS and sub.tag in self.BLOCK_TAGS :
+                    stream.write('\n\n')
             self.render_element(sub, stream, indent)
+            prev_tag = sub.tag
+
         stream.write(end)
         if element.tail:
+            if element.tag in self.BLOCK_TAGS:
+                element.tail = element.tail.rstrip('\n')
             stream.write(element.tail)
 
     def render_element(self, element: Element, stream: StringIO, indent: int) -> None:
-        if element.tag == 'li' and element.text == '\n':
-            element.text = None
-        elif element.tag == 'li' and element.tail == '\n' and len(element) and list(element)[-1].tag in ('ul', 'ol'):
-            element.tail = None
-        elif element.tag in ('ul', 'ol') and element.tail == '\n' and indent > 0:
-            element.tail = None
-
-        if element.tag == 'pre':
-            stream.write(self.render_code_block(element))
-        elif element.tag in ('ul', 'ol'):
+        if element.tag == "pre":
+            stream.write(self.render_code_block(element).rstrip('\n'))
+            if element.tail:
+                stream.write(element.tail.rstrip('\n'))
+        elif element.tag in ("ul", "ol"):
             liststream = StringIO()
             self.render_basic_element(element, liststream, indent + 1)
             lines = liststream.getvalue().splitlines(keepends=True)
