b444c686c7af981e9f3c24562bbcd173fef4a394
Mitchell Goff <mitchellgoffpc@gmail.com>
2025-08-22 00:23:35 -0700

    Added Diff component

---
+++ ask/ui/diff.py
@@ -0,0 +1,44 @@
+import difflib
+
+from ask.ui.components import Component, Text, Box
+from ask.ui.styles import Colors, Theme
+
+
+def Diff(old: str, new: str) -> Component:
+    old_lines = old.splitlines(keepends=True)
+    new_lines = new.splitlines(keepends=True)
+    diff = list(difflib.unified_diff(old_lines, new_lines, n=3))
+
+    components: list[Component] = []
+    old_line_num, new_line_num = 1, 1
+    for line in diff[2:]:  # Skip header lines
+        delta = line[1:].rstrip('\n')
+        if line.startswith('@@'):
+            parts = line.rstrip('\n').split()
+            if len(parts) >= 2:
+                old_range = parts[1][1:].split(',')
+                new_range = parts[2][1:].split(',')
+                old_line_num = int(old_range[0])
+                new_line_num = int(new_range[0])
+            if components:
+                components.append(Text(f"{Colors.hex(' ... ', Theme.GRAY)}"))
+        elif line.startswith('-'):
+            line_num = Colors.hex(f"{old_line_num:>4}", Theme.GRAY)
+            delta = Colors.bg_hex(Colors.hex(f'-  {delta}', Theme.WHITE), Theme.DARK_RED)
+            components.append(Text(f"{line_num} {delta}"))
+            old_line_num += 1
+        elif line.startswith('+'):
+            line_num = Colors.hex(f"{new_line_num:>4}", Theme.GRAY)
+            delta = Colors.bg_hex(Colors.hex(f'+  {delta}', Theme.WHITE), Theme.DARK_GREEN)
+            components.append(Text(f"{line_num} {delta}"))
+            new_line_num += 1
+        elif line.startswith(' '):
+            line_num = Colors.hex(f"{old_line_num:>4}", Theme.GRAY)
+            delta = Colors.hex(f'   {delta}', Theme.WHITE)
+            components.append(Text(f"{line_num} {delta}"))
+            old_line_num += 1
+            new_line_num += 1
+        if not line.endswith('\n'):
+            components.append(Text(f"{line_num} \\ No newline at end of file"))
+
+    return Box()[components]
