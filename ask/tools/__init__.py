e4e8885167020a02d852f76b9955c3ae8617aa74
Mitchell Goff <mitchellgoffpc@gmail.com>
2025-09-11 22:03:40 -0700

    Add ToDo tool to main tool list

--- ask/tools/__init__.py
+++ ask/tools/__init__.py
@@ -7,12 +7,15 @@ from ask.tools.list import ListTool
 from ask.tools.multi_edit import MultiEditTool
 from ask.tools.python import PythonTool
 from ask.tools.read import ReadTool
+from ask.tools.todo import ToDoTool
 from ask.tools.write import WriteTool
 
-TOOL_LIST = [BashTool(), EditTool(), GlobTool(), GrepTool(), ListTool(), MultiEditTool(), PythonTool(), ReadTool(), WriteTool()]
+TOOL_LIST = [BashTool(), EditTool(), GlobTool(), GrepTool(), ListTool(), MultiEditTool(), PythonTool(), ReadTool(), ToDoTool(), WriteTool()]
 TOOLS = {tool.name: tool for tool in TOOL_LIST}
 
-__all__ = ["TOOLS", "Tool", "ToolError", "ToolCallStatus", "Parameter"]
+__all__ = [
+    "TOOLS", "Tool", "ToolError", "ToolCallStatus", "Parameter",
+    "BashTool", "EditTool", "GlobTool", "GrepTool", "ListTool", "MultiEditTool", "PythonTool", "ReadTool", "ToDoTool", "WriteTool"]
 
 
 if __name__ == "__main__":
